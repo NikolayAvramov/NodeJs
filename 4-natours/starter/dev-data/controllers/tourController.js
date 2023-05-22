9db91eff2e9e6f0b470e2929cc2c5ac7e9fb888b
Nikolay <106226408+NikolayAvramov@users.noreply.github.com>
2023-05-22 22:28:55 +0300

    add pagination

--- 4-natours/starter/dev-data/controllers/tourController.js
+++ 4-natours/starter/dev-data/controllers/tourController.js
@@ -29,28 +29,39 @@ exports.getAllTours = async (req, res) => {
     const queryObj = { ...req.query };
     const excludedFiles = ['page', 'sort', 'limit', 'fields'];
     excludedFiles.forEach((el) => delete queryObj[el]);
-// filtering
+    // filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-   
+
     let query = Tour.find(JSON.parse(queryStr));
     // sorting
-if(req.query.sort){
-  const sortByQuery = req.query.sort.split(",").join(" ")
-query = query.sort(sortByQuery)
-}else{
-query = query.sort("-createdAt")
-}
+    if (req.query.sort) {
+      const sortByQuery = req.query.sort.split(',').join(' ');
+      query = query.sort(sortByQuery);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
-//fields limiting
-if(req.query.fields){
-  const fields = req.query.fields.split(",").join(" ")
-  query = query.select(fields)
-}else{
-  query = query.select("-__v")
+    //fields limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+    //pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
 
-}
+    query.skip(skip).limit(limit);
 
+    if (req.query.page) {
+      const numTour = await Tour.countDocuments();
+      if (skip >= numTour) {
+        throw new Error('This page does not exist');
+      }
+    }
     const tours = await query;
 
     res.status(200).json({
